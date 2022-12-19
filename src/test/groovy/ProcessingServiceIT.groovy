74a6ed7e10c993e2763765052f983610bef45202
Dominik <czlowiekprosty@gmail.com>
2022-12-19 16:16:51 +0100

    change to groovy

---
+++ src/test/groovy/ProcessingServiceIT.groovy
@@ -0,0 +1,12 @@
+import spock.lang.Specification
+
+class ProcessingServiceIT extends Specification {
+
+     def "test"() {
+
+        def string = "test"
+        def result = "test"
+        expect:
+        1 == 1
+    }
+}
