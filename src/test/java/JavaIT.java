44d3f5a6ce1d0de48cc3264e41dcbeeb785b13a2
Dominik <czlowiekprosty@gmail.com>
2022-12-22 01:51:28 +0100

    refactor java integration test

--- src/test/java/JavaIT.java
+++ src/test/java/JavaIT.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
@@ -19,42 +20,46 @@ import java.util.Properties;
 @EmbeddedKafka(
         partitions = 1,
         topics = {
-                "inbox-topic",
-                "outbox-topic"
+                "${spring.kafka.test.inbox-topic}",
+                "${spring.kafka.test.outbox-topic}"
         },
         brokerProperties = {
-                "listeners=PLAINTEXT://127.0.0.1:9092"
+                "listeners=PLAINTEXT://${spring.kafka.test.bootstrap-servers}"
         }
 )
 @SpringBootTest(classes = {KafkaConfig.class, ProcessingService.class})
 public class JavaIT {
 
-        private String localhost = "127.0.0.1:9092";
-        private String inboxTopic = "inbox-topic";
-        private String outboxTopic = "outbox-topic";
-        private String groupId = "group-id";
-        private String offset = "earliest";
-
+        @Value("${spring.kafka.test.bootstrap-servers}")
+        private String bootstrapServers;
+        @Value("${spring.kafka.test.inbox-topic}")
+        private String inboxTopic;
+        @Value("${spring.kafka.test.outbox-topic}")
+        private String outboxTopic;
+        @Value("${spring.kafka.test.group-id}")
+        private String groupId;
+        @Value("${spring.kafka.test.auto-offset}")
+        private String offset;
         @Autowired
         ProcessingService service;
-
         @Autowired
         KafkaConfig kafkaConfig;
 
         @Test
         public void shouldPollProcessedMessageWhenEventIsSentToKafka() {
+                var expected = "nulla dies sine linea";
                 KafkaProducer kafkaProducer = new KafkaProducer(createProducerProps());
                 KafkaConsumer kafkaConsumer = new KafkaConsumer(createConsumerProps());
                 kafkaConsumer.subscribe(List.of(outboxTopic));
 
-                kafkaProducer.send(new ProducerRecord(inboxTopic, "msg"));
+                kafkaProducer.send(new ProducerRecord(inboxTopic, expected));
 
-                assert KafkaTestUtils.getSingleRecord(kafkaConsumer, outboxTopic).value().equals("msg");
+                assert KafkaTestUtils.getSingleRecord(kafkaConsumer, outboxTopic).value().equals(expected);
         }
 
         Properties createConsumerProps() {
                 var props = new Properties();
-                props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, localhost);
+                props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
                 props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
                 props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
                 props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
@@ -64,7 +69,7 @@ public class JavaIT {
 
         Properties createProducerProps() {
                 var props = new Properties();
-                props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, localhost);
+                props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
                 props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
                 props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
                 return props;
